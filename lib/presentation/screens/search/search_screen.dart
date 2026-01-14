42654279372f7220791811fe857ec7253d3dbe0d
mohammed afreed <mohammedafreedps111@gmail.com>
2026-01-14 21:47:42 +0530

    search popup swipe added

--- lib/presentation/screens/search/search_screen.dart
+++ lib/presentation/screens/search/search_screen.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:myydoctor/presentation/screens/profile/profile_screen.dart';
 import 'package:myydoctor/presentation/widgets/colours.dart';
-import 'package:myydoctor/presentation/widgets/search/dropdown.dart';
 import 'package:myydoctor/services/doctors_list.dart';
 import 'package:myydoctor/services/location/doctors_location.dart';
 import 'package:myydoctor/services/location/location.dart';
@@ -22,6 +22,7 @@ class _SearchScreenState extends State<SearchScreen> {
   String? selectedSpecialization;
   String searchQuery = "";
   bool showSwipes = false;
+  Timer? showSwipesin;
 
   final DoctorsList doctorsList = DoctorsList();
   List<Map<String, String>> filteredDoctors = [];
@@ -130,32 +131,62 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    showSwipesin = Timer(Duration(seconds: 3), () {
+      setState(() {
+        showSwipes = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    if (showSwipesin != null) {
+      showSwipesin!.cancel();
+      showSwipesin = null;
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
       appBar: AppBar(
         actions: [
           GestureDetector(
             onTap: () {
-              showDialog(context: context, builder: (context) => Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: AlertDialog(
-                  insetPadding: EdgeInsets.zero,
-                  contentPadding: EdgeInsets.zero,
-                  content: Container(
-                    height: MediaQuery.of(context).size.height,
-                    width: double.infinity,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(15),
-                      child: Image.asset("assets/images/searchswipes0.jpg", fit: BoxFit.cover,))),
-                ),
-              ),);
+              showDialog(
+                context: context,
+                builder:
+                    (context) => Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: AlertDialog(
+                        insetPadding: EdgeInsets.zero,
+                        contentPadding: EdgeInsets.zero,
+                        content: Container(
+                          height: MediaQuery.of(context).size.height,
+                          width: double.infinity,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.asset(
+                              "assets/images/searchswipes0.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+              );
             },
             child: Padding(
               padding: const EdgeInsets.only(right: 15),
-              child: Icon(Icons.format_indent_decrease_rounded, color: Colors.amber,),
+              child: Icon(
+                Icons.format_indent_decrease_rounded,
+                color: Colors.amber,
+              ),
             ),
-          )
+          ),
         ],
         backgroundColor: Color(0xFF1F323C),
         automaticallyImplyLeading: false,
@@ -215,60 +246,208 @@ class _SearchScreenState extends State<SearchScreen> {
             width: double.infinity,
             child: Image.asset(
               "assets/images/searchswipes1.png",
-              fit: BoxFit.cover
+              fit: BoxFit.cover,
             ),
           ),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              decoration: BoxDecoration(
-                color: AppColors.grey,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              width: MediaQuery.of(context).size.width * 0.85,
-              height: MediaQuery.of(context).size.height * 0.75,
-              child: Padding(padding: EdgeInsetsGeometry.all(10),child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.79,
-                        height: MediaQuery.of(context).size.height * 0.35,
-                        decoration: BoxDecoration(
-                          color: AppColors.buttonBackground,
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_drop_down_circle_sharp,size: 40,color: Colors.white,)),
-                        )
-                      ),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: EdgeInsetsGeometry.symmetric(horizontal:  15, vertical: 20),
-                          child: Column(
-                            children: [
-                              Container(
-                                width: 10,
-                                height: 10,
-                                decoration: BoxDecoration(shape: BoxShape.circle,color: Colors.white),
+          showSwipes
+              ? Align(
+                alignment: Alignment.center,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  width: MediaQuery.of(context).size.width * 0.85,
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: Padding(
+                    padding: EdgeInsetsGeometry.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.79,
+                              height: MediaQuery.of(context).size.height * 0.35,
+                              decoration: BoxDecoration(
+                                color: AppColors.buttonBackground,
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            ],
-                          ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 5,
+                                ),
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      showSwipes = false;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.arrow_drop_down_circle_sharp,
+                                    size: 40,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Padding(
+                                padding: EdgeInsetsGeometry.symmetric(
+                                  horizontal: 15,
+                                  vertical: 20,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      width: 10,
+                                      height: 10,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  )
-                ],
-              ),),
-            ),
-          )
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Dr.Felix, MBBS MD DM Cardiology',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 20),
+                                  Row(
+                                    children: [
+                                      Image.asset(
+                                        'assets/images/verified_icon.png',
+                                      ),
+                                      SizedBox(width: 10),
+                                      Stack(
+                                        children: [
+                                          Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Container(
+                                              alignment: Alignment.centerRight,
+                                              width: 140,
+                                              height: 27,
+                                              decoration: BoxDecoration(
+                                                color: Colors.grey[100],
+                                                borderRadius:
+                                                    BorderRadius.circular(30),
+                                              ),
+                                              child: Padding(
+                                                padding: EdgeInsetsGeometry.only(right: 10),
+                                                child: Text(
+                                                  
+                                                  'Heart Failure Specialist',
+                                                  style: TextStyle(fontSize: 10),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                          Image.asset(
+                                            'assets/images/golden_coin_icon.png',
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10,),
+                            Container(
+                              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.grey[400]
+                              ),
+                              
+                              child: Column(
+                                children: [
+                                  // Image.asset('assets/images/star.png'),
+                                  Text('1216',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 25)),
+                                  Text('Carm',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.1,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                
+                              ),
+                              child: Image.asset('assets/images/x_icon.png'),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                
+                              ),
+                              child: Image.asset('assets/images/message_icon.png'),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                
+                              ),
+                              child: Image.asset('assets/images/red_star.png'),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                
+                              ),
+                              child: Image.asset('assets/images/stethoscope_icon.png'),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey,
+                                
+                              ),
+                              child: Image.asset('assets/images/heart_icon.png'),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+              : SizedBox.shrink(),
         ],
-      )
+      ),
     );
   }
 
